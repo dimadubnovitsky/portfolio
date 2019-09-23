9e40901ce2fec60508a84d9f60db6a457bf66776
Dima Dubnovitsky <dimadubnovitsky@gmail.com>
2019-09-23 11:38:04 +0300

    use redux-form

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -3,22 +3,31 @@ import styles from './Contacts.module.css';
 import BlockTitle from "../BlockTitle/BlockTitle";
 import Fade from 'react-reveal/Fade';
 import {Slide} from "react-reveal";
+import {Field, reduxForm} from "redux-form";
+
+const ContactsForm = () => {
+    return (
+        <form className={styles.formWrapper}>
+            <Slide bottom>
+                <Field className={styles.formArea} name="name" placeholder="Name" type="text" component="input"/>
+                <Field className={styles.formArea} name="email" placeholder="E-mail" type="email" component="input"/>
+                <Field className={styles.messageArea} name="message" placeholder="Message" component="textarea"/>
+            </Slide>
+            <Fade bottom>
+                <button className={styles.btnSubmit} type="submit">Send</button>
+            </Fade>
+        </form>
+    )
+}
+
+const ContactsFormRedux = reduxForm({form: "ContactsFrom"})(ContactsForm)
 
 const Contacts = () => {
     return (
         <div id={"contacts"} className={styles.contacts}>
             <div className={styles.container}>
                 <BlockTitle title={"Contacts"}/>
-                <form className={styles.formWrapper}>
-                    <Slide bottom>
-                        <input className={styles.formArea} placeholder="Name" type="text"/>
-                        <input className={styles.formArea} placeholder="E-mail" type="email"/>
-                        <textarea className={styles.messageArea} placeholder="Message"/>
-                    </Slide>
-                    <Fade bottom>
-                        <button className={styles.btnSubmit} type="submit">Send</button>
-                    </Fade>
-                </form>
+                <ContactsFormRedux/>
             </div>
         </div>
     );
